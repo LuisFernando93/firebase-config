4ca665547a94826785b6c1563dac8a22fd14f6b7
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2019-06-15 14:44:46 -0300

    Projeto Final

--- Firebase configuracao/ViewController.swift
+++ Firebase configuracao/ViewController.swift
@@ -7,12 +7,64 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textoLabel: UILabel!
+    
+    let firebase = Database.database().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /*
+        do {
+            try Auth.auth().signOut()
+        } catch  {
+            print("Erro ao deslogar o usuario")
+        }
+        */
+        
+        Auth.auth().signIn(withEmail: "luispasquinelli@hotmail.com", password: "123456") { (usuario, erro) in
+            if erro == nil{
+                print("sucesso ao logar o usuario")
+            } else {
+                print("erro ao logar usuario: \(erro?.localizedDescription)")
+            }
+        }
+ 
+        Auth.auth().addStateDidChangeListener { (Auth, usuario) in
+            
+            if let usuarioLogado = usuario {
+                print("usuario esta logado. email:" + String(describing: usuarioLogado.email))
+            } else {
+                print("usuario nao esta logado")
+            }
+        }
+        
+        /*
+        Auth.auth().createUser(withEmail: "luispasquinelli@hotmail.com", password: "123456") { (usuario, erro) in
+       
+            if erro == nil{
+                print("sucesso ao cadastrar o usuario")
+            } else {
+                print("erro ao cadastrar usuario: \(erro?.localizedDescription)")
+            }
+        }
+        */
+ 
+        let pontuacao = firebase.child("pontuacao").child("valor")
+        //pontuacao.removeValue()
+        // pontuacao.child("003").child("nome").setValue("Luis")
+        //pontuacao.child("valor").setValue("100")
+        
+        //Recuperar dados do firebase
+        pontuacao.observe(DataEventType.value, with: {(dados) in
+            let ponto = dados.value as! String
+            self.textoLabel.text = ponto
+        })
     }
 
     override func didReceiveMemoryWarning() {
